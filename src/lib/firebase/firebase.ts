71d9dd237dd909e522ce0ba1b8156e948d3208c6
Patrick Enaholo <patxiworks@gmail.com>
2025-04-24 21:29:12 +0000

    Fix FirebaseError: Failed to get document because the client is offline.

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, enableIndexedDbPersistence } from 'firebase/firestore';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -17,6 +17,11 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-console.log(app)
 
 export const db = getFirestore(app);
+
+// Enable offline persistence
+enableIndexedDbPersistence(db)
+  .catch((err) => {
+    console.error("Failed to enable offline persistence:", err);
+  });
